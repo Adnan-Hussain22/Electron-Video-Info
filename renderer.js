ab7cea5029e526ab1b4032faa2980ca1d52133ae
Adnan-Hussain22 <adnanrajput42@gmail.com>
2019-07-26 03:48:27 +0500

    display result

--- renderer.js
+++ renderer.js
@@ -8,5 +8,5 @@ document.querySelector('form').addEventListener('submit', (e) => {
 });
 
 ipcRenderer.on('video:duration',(e,duration)=>{
-    console.log('duration===>',duration)
+    document.querySelector('#result').innerHTML = `The video is ${(duration/60).toFixed(2)} minutes`
 })
